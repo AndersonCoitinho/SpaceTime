6ebf4bd8768a89dc14b581f3c15f220f8a4a8211
Anderson Coitinho <104800828+AndersonCoitinho@users.noreply.github.com>
2023-05-17 16:47:29 -0300

    Iniciando front web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,29 @@
 export default function Home() {
-  return <div className="text-2xl uppercase">oii</div>
+  return (
+    <main className="grid min-h-screen grid-cols-2">
+      {/* Left */}
+      <div className="relative flex flex-col items-start justify-between overflow-hidden bg-[url(../assets/bg-stars.svg)] bg-cover px-28 pt-16">
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full border-r border-white/10 bg-purple-700 opacity-50 blur-full" />
+
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+        <a href="" className="flex items-center gap-3 text-left">
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400"></div>
+        </a>
+      </div>
+
+      {/* Right */}
+      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[300px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
+  )
 }
